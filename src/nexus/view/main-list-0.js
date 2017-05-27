8310144a2f0d2a4e15a613d6628cddecccc72264
raoul2000 <manu34@free.fr>
2017-05-27 20:36:41 +0200

    refactor event handler for tbody

--- src/nexus/view/main-list-0.js
+++ src/nexus/view/main-list-0.js
@@ -1,17 +1,15 @@
 "use strict";
 const ipcRenderer = require('electron').ipcRenderer;
 
+
 $('tbody').on('click',function(ev){
-
-  let $target    = $(ev.target);            // the HTML element clicked
-  let row        = $target.closest('tr');   // the TR wrapper (row)
-  let moduleId   = row.prop('id');          // the module Id
-  let modVersion = row.data('version');     // module version data (or null)
-
+  let $target    = $(ev.target);
+  let row        = $target.closest('tr');
+  let moduleId   = row.prop('id');
+  let modVersion = row.data('version');
   console.log(moduleId);
-  //////////////////////////////////////////////////////////////////////////////
+
   if( $target.closest('.but-download-cancel').length > 0 ) {
-    // usser cancel the current download (in progress)
     console.log('but-download-cancel');
     let $button = $target.closest('.but-download-cancel').first();
 
@@ -25,7 +23,6 @@ $('tbody').on('click',function(ev){
 
   } else  //////////////////////////////////////////////////////////////////////
     if( $target.closest('.chk-module').length > 0 ) {
-    // User is asking to load version info for this module
     // if version info has not already been retreieved, fetch it now
     //
     console.log('chk-module');
@@ -41,7 +38,6 @@ $('tbody').on('click',function(ev){
 
   } else ///////////////////////////////////////////////////////////////////////
     if( $target.closest('.but-download-start').length > 0 ) {
-      // user starts the download of this module and for the selected version
       console.log('.but-download-start');
       let $button = $target.closest('.but-download-start').first();
 
@@ -65,7 +61,7 @@ $('tbody').on('click',function(ev){
 });
 
 $('tbody').on('change',function(ev){
-
+  console.log('cHANGE');
   let $target = $(ev.target);
   let row = $target.closest('tr');
   let moduleId = row.prop('id');
@@ -87,9 +83,7 @@ $('tbody').on('change',function(ev){
   }
 });
 
-//////////////////////////////////////////////////////////////////
-// Custom Event handlers
-//
+
 ipcRenderer.on('nx-download-mod.progress', function(sender, data){
     console.log(data);
     let row = $('#'+data.moduleId);
@@ -100,9 +94,83 @@ ipcRenderer.on('nx-download-mod.progress', function(sender, data){
     row.find('.progress-percent').text(percent + "%");
 });
 
+$('.but-download-cancel____').on('click',function(){
+  let $button = $(this);
+  let row = $button.closest('tr');
+  let modId = row.prop('id');
+
+  // update UI
+  row.find('.download-progress').hide();
+  row.find('.progress-percent').text("");
+  $button.prop('disabled', true);
+  row.find('.but-download-start').prop('disabled', false);
+
+  ipcRenderer.send('nx-download-mod.cancel',{ moduleId : modId });
+});
+
+$('.but-download-start___').on('click',function(el){
+  let $button = $(this);
+  let row = $button.closest('tr');
+  let modId = row.prop('id');
+  let modVersion = row.data('version');
+
+  // update UI
+  row.find('.download-progress').show();
+  $button.prop('disabled', true);
+  row.find('.but-download-cancel').prop('disabled', false);
+
+  console.log(modVersion);
+
+  let selVersion = row.find('.sel-version-val > option:selected').prop('value');
+  let selCat     = row.find('.sel-version-cat > option:selected').prop('value');
+  let arg = {
+    moduleId : modId,
+    version : selVersion,
+    cat : selCat
+  };
+  console.log(arg);
+  ipcRenderer.send('nx-download-mod.start',arg);
+});
+
 ipcRenderer.on('nx-fetch-version.done',function(event,data){
   let row = $('#'+data.moduleId);
   row.data('version',data.version);
   row.find('.sel-version-cat').trigger('change');
   row.removeClass().addClass('loaded');
+});
+
+$('.chk-module___').on('click',function(el){
+  let $checkbox = $(this);
+  if( $checkbox.prop('checked') ) {
+    let row = $checkbox.closest('tr');
+    let modVersion = row.data('version');
+    if( ! modVersion ) {
+      let id = row.prop('id');
+      console.log("module id : "+id);
+      // get module version data
+      console.log("downloading version data for module "+id);
+      row.removeClass().addClass('loading-version');
+
+      ipcRenderer.send('nx-fetch-version.start',{ moduleId : id});
+    }
+  } else {
+
+  }
+});
+
+
+$('.sel-version-cat__').on('change',function(el){
+  console.log('changing');
+  let $selCat = $(this);
+  let row =$selCat.closest('tr');
+  let $selVersion = row.find('.sel-version-val');
+  let selectedOption = $selCat.find('option:selected').prop('value');
+
+  // clean and then populate version select list
+  $selVersion.children('option').remove();
+  row.data('version')[selectedOption].forEach(function(optValue){
+    $selVersion.append($("<option></option>")
+     .attr("value",optValue)
+     .text(optValue));
+  });
 });
