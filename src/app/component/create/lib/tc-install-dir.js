ede37bd9ae61e65897af263fc0e866cdf0a0cc0e
raoul2000 <manu34@free.fr>
2017-09-16 14:53:52 +0200

    comment

--- src/app/component/create/lib/tc-install-dir.js
+++ src/app/component/create/lib/tc-install-dir.js
@@ -3,14 +3,18 @@
 /**
  * Finds the Tomcat install folder name for a specific tomcat ID.
  * The path value returned is relative to the SSH user home directoy.
- * 
+ *
+ * This function assumes that the following convention applies :
+ * being given a string (the tomcat id), the folder where the tomcat is installed
+ * patches the patern '*tomcat-[tomcat ID]'.
+ *
  * returns : {
  *  "id" : "tc id",
  *  "installDir" : "/path/to/tomcat/install/dir"
  * }
- * @param  {[type]} options   [description]
- * @param  {[type]} tomcatId [description]
- * @return {[type]}          [description]
+ * @param  {object} options   a valid SSH2 object instance
+ * @param  {string} tomcatId the tomcat id
+ * @return {object}          object describing the result
  */
 function extractInstallDir(ssh, tomcatId, path) {
 
