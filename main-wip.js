dee5b1c84ae7a6d323dede3b0eac265ac478b0bd
raoul2000 <manu34@free.fr>
2017-10-28 14:25:39 +0200

    review config managment

--- main-wip.js
+++ main-wip.js
@@ -1,6 +1,8 @@
 const electron = require('electron');
 // Module to control application life.
 const app = electron.app;
+app.setName('My-Toolbox');
+
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
 
--- src/app/service/config.js
+++ src/app/service/config.js
@@ -5,17 +5,27 @@
 var app = require('electron').remote.app;
 // see https://github.com/sindresorhus/electron-store
 const Store = require('electron-store');
+const path = require('path');
 
 const store = new Store({
   "name" : "my-toolbox",
   "defaults" : {
-    "dataFolder" : app.getPath('home'),
-    // 'deployFolder' : absolute path of the folder where  all the artefact files
-    // candidate for deployment are stored.
-    "deployFolder" : app.getPath('home')
+    "appDataPath" : app.getPath('userData'), // on windows C:\Users\Utilisateur\AppData\Roaming\<appName>
+    "ctdbPath"    : path.join(app.getPath('userData'),'ctbd'),
+    "dataFolder"  : app.getPath('home'),
+    // 'deployFolder' : absolute path of the folder where  modules are :
+    // - downloaded from maven
+    // - uploaded on deployement
+    "deployFolder" : app.getPath('downloads')
   }
 });
 console.log("loading config from ", store.path);
+console.log("version ", app.getVersion());
+console.log("name ", app.getName());
+console.log("appData ", app.getPath('appData'));
+console.log("userData ", app.getPath('userData'));
 
 
+
+//module.exports = store;
 module.exports = store;
