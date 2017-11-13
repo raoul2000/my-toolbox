e51e9b8b29e4097eb1a89c47ca4f8a9623a4d83f
raoul2000 <manu34@free.fr>
2017-11-13 11:25:46 +0100

    style desktop (wip)

--- src/app/component/desktop/main.html.js
+++ src/app/component/desktop/main.html.js
@@ -6,31 +6,48 @@ module.exports = `
       <h1>Desktop</h1>
       <hr/>
       <div class="btn-group" role="group" style="margin-bottom:1em;">
-        <button @click="openDesktopItems" type="button" class="btn btn-primary">
+        <button
+          @click="openDesktopItems"
+          title="open ..."
+          type="button" class="btn btn-default">
           <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
         </button>
-        <button @click="createItem" type="button" class="btn btn-default">
+        <button
+          @click="createItem"
+          title="create item"
+          type="button" class="btn btn-default">
           <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
         </button>
       </div> <!-- toolbar end -->
     </div>
   </div>
 
-
-    <div class="row">
-      <div class="col-xs-4">
-        <div class="dk-item">
-          <h3><b>TEST</b> / <span>CAT1</span></h3>
-        </div>
-      </div>
-    </div>
-
   <div class="row">
-    <div class="col-xs-4" v-for="(item, index)  in items" :title="item.file">
-      <h3 class="title">{{ item.data.name }}</h3>
-      <p>(index = {{index}})</p>
-      <button @click="removeFromDesktop(index)">remove</button>
-      <button @click="view(index)">view</button>
+    <div class="col-xs-12">
+      <ul class="cards">
+        <li
+          v-for="(item, index)  in items"
+          :title="item.file"
+          class="card_item">
+
+          <div class="card">
+    					<div class="card_content">
+    							<h2 class="card_heading" :title="item.data.name">{{ item.data.name }}</h2>
+
+                  <div class="btn-group btn-group-sm" role="group">
+                    <button
+                      @click="removeFromDesktop(index)" type="button" class="btn btn-sm btn-default">
+                      <span class="glyphicon glyphicon-remove" aria-hidden="true"></span>
+                    </button>
+                    <button @click="view(index)" type="button" class="btn btn-sm btn-default">
+                      <span class="glyphicon glyphicon-eye-open" aria-hidden="true"></span>
+                    </button>
+                  </div> <!-- toolbar end -->
+    					</div>
+    			</div>
+
+        </li>
+      </ul>
     </div>
   </div>
 
