42a98f5f11a5fa7cdaa431e2027472e9647da854
raoul2000 <manu34@free.fr>
2017-11-29 16:35:23 +0100

    test sliding page (wip)

--- src/app/component/desktop/main.html.js
+++ src/app/component/desktop/main.html.js
@@ -1,6 +1,13 @@
 module.exports = `
 <div>
 
+
+<div id="pt-main" class="pt-perspective">
+	<div class="pt-page">
+		<h1><span>A collection of</span><strong>Page</strong> Transitions</h1>
+	</div>
+</div>
+
   <div class="row">
     <div class="col-lg-12">
       <h1>Desktop</h1>
