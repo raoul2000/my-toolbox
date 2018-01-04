ba45c1bba59116f188a953f1d144444a883e236d
raoul2000 <manu34@free.fr>
2018-01-04 18:24:27 +0100

    refactore smart command to use passed ssh instance

--- src/app/lib/ssh/smart-command.js
+++ src/app/lib/ssh/smart-command.js
@@ -3,14 +3,16 @@
 const NodeSSH = require('node-ssh');
 
 function parseStdout(stdout, type) {
-  let actualType = type || ( stdout.lastIndexOf('\n') === -1 ? "single" : "list");
-  let parsedResult = stdout;
-  console.log("actualType = ",actualType);
+  let actualType = type || ( stdout.lastIndexOf('\n') === -1 ? "string" : "list");
+  let parsedResult = null;
+  //console.log("actualType = ",actualType);
   if( actualType === "list") {
     parsedResult = stdout
       .split('\n')
       .map( item => item.trim())
       .filter( item => item.length !== 0);
+  } else if( actualType === 'string'){
+    parsedResult = stdout;
   } else {
     throw new Error('unsupported result type : '+actualType);
   }
@@ -20,32 +22,14 @@ function parseStdout(stdout, type) {
 /** -----------------------------------------------------------------
  * options :
  * {
- *    ssh : {
- *      'host' : '127.0.0.1',
- *      'port' : 22,
- *      'username' : 'user',
- *      'password' : '*****'
- *    },
  *    'command' : "set -o pipefail; ls -d -- tomcat/ | cut -d etc ....",
  *    'resultType' : "single" | "list"
   * }
  */
-exports.run = function(options) {
-  let ssh = new NodeSSH();
-  return ssh
-    .connect(options.ssh)
-    .then(() => {
-      console.log('bip');
-      return ssh.execCommand(options.command,[],{stream: 'stdout'});
-    })
-    .then(( commandResult ) => {
-      ssh.dispose();
-      commandResult.value = parseStdout(commandResult.stdout, options.resultType);
-      return commandResult;
-    })
-    .catch(err => {
-      ssh.dispose();
-      throw new Error(err);
-    }
-  );
+exports.run = function(nodessh, options) {
+  return nodessh.execCommand(options.command,[],{stream: 'stdout'})
+  .then(( commandResult ) => {
+    commandResult.value = parseStdout(commandResult.stdout, options.resultType);
+    return commandResult;
+  });
 };
--- test/smart-command.js
+++ test/smart-command.js
@@ -1,8 +1,7 @@
 "use strict";
 
-var fs = require('fs'),
-  node_ssh = require('node-ssh'),
-  path = require('path'),
+const fs = require('fs'),
+  NodeSSH = require('node-ssh'),
   smartCommand = require('../src/app/lib/ssh/smart-command'),
   assert = require('chai').assert;
 
@@ -15,70 +14,124 @@ describe('smart command', function(done) {
     config = JSON.parse(fs.readFileSync(__dirname + "/config-stage.json", "utf-8"));
   });
 
-  it('fails if incorrect connection info are provided', function(done) {
-    smartCommand.run({
-      "ssh" : null
+  it('returns an error code on invalid command', function(done) {
+    let ssh = new NodeSSH();
+    ssh.connect(config.ssh)
+    .then( () => {
+      return smartCommand.run(ssh,{
+        "command" : `INVALID COMMAND`
+      });
     })
-    .then(() => {
-      done(new Error("should fail"));
-    })
-    .catch(err => {
-      done();
-    });
-  });
-
-  it('fails if connection credentials are wrong', function(done) {
-    smartCommand.run({
-      "ssh" : {
-        "host": "127.0.0.1",
-        "port" : 22,
-        "username": "root",
-        "password" : "invalid_password"
-      }
-    })
-    .then(() => {
-      done(new Error("should fail"));
-    })
-    .catch(err => {
-      done();
-    });
-  });
-
-  it('runs a simple remote command', function(done) {
-    smartCommand.run({
-      "ssh" : config.ssh,
-      "command" : `ls -l`,
-      //"command" : `. .bash_profile; set -o pipefail; cat $HOME/cfg/eomvar.dtd | grep TOMCAT_ | cut -d ' ' -f 2 | cut -d '_' -f 2 | sort > $TMPDIR/$$.tmp && uniq $TMPDIR/$$.tmp && rm $TMPDIR/$$.tmp`,
-      "resultType" : "list"
-    })
-    .then((result) => {
-      console.log(result);
-      assert.equal(result.code,0);
+    .then((commandResult) => {
+      //console.log(commandResult);
+      ssh.dispose();
+      assert.isFalse(commandResult.code === 0);
+      assert.isString(commandResult.value);
+      assert.isNotEmpty(commandResult.stderr);
+      assert.isEmpty(commandResult.stdout);
+      assert.isUndefined(commandResult.signal);
       done();
     })
     .catch(err => {
       console.log(err);
+      ssh.dispose();
       done(err);
     });
   });
 
-  it('run a simple remote command', function(done) {
-    smartCommand.run({
-      "ssh" : config.ssh,
-      "command" : `ls -l`,
-      //"command" : `. .bash_profile; set -o pipefail; cat $HOME/cfg/eomvar.dtd | grep TOMCAT_ | cut -d ' ' -f 2 | cut -d '_' -f 2 | sort > $TMPDIR/$$.tmp && uniq $TMPDIR/$$.tmp && rm $TMPDIR/$$.tmp`,
-      "resultType" : "list3"
+  it('runs a simple remote command that returns a string', function(done) {
+    let ssh = new NodeSSH();
+    ssh.connect(config.ssh)
+    .then( () => {
+      return smartCommand.run(ssh,{
+        "command" : `ls -l`,
+        "resultType" : "string"
+      });
     })
-    .then((result) => {
-      console.log(result);
-      assert.equal(result.code,0);
+    .then((commandResult) => {
+      //console.log(commandResult);
+      ssh.dispose();
+      assert.equal(commandResult.code,0);
+      assert.isString(commandResult.value);
+      assert.isNotEmpty(commandResult.stdout);
+      assert.isEmpty(commandResult.stderr);
+      assert.isUndefined(commandResult.signal);
       done();
     })
     .catch(err => {
       console.log(err);
+      ssh.dispose();
       done(err);
     });
   });
 
+  it('parses result as an array if it contains newline', function(done) {
+    let ssh = new NodeSSH();
+    ssh.connect(config.ssh)
+    .then( () => {
+      return smartCommand.run(ssh,{
+        "command" : `ls -l`
+      });
+    })
+    .then((commandResult) => {
+      //console.log(commandResult);
+      ssh.dispose();
+      assert.isArray(commandResult.value);
+      done();
+    })
+    .catch(err => {
+      console.log(err);
+      ssh.dispose();
+      done(err);
+    });
+  });
+
+  it('parses result as string if it does not contains newline', function(done) {
+    let ssh = new NodeSSH();
+    ssh.connect(config.ssh)
+    .then( () => {
+      return smartCommand.run(ssh,{
+        "command" : `whoami`
+      });
+    })
+    .then((commandResult) => {
+      //console.log(commandResult);
+      ssh.dispose();
+      assert.isString(commandResult.value);
+      done();
+    })
+    .catch(err => {
+      console.log(err);
+      ssh.dispose();
+      done(err);
+    });
+  });
+
+  it('runs a simple remote command that returns an array', function(done) {
+    let ssh = new NodeSSH();
+    ssh.connect(config.ssh)
+    .then( () => {
+      return smartCommand.run(ssh,{
+        "command" : `ls -l`,
+        //"command" : `. .bash_profile; set -o pipefail; cat $HOME/cfg/eomvar.dtd | grep TOMCAT_ | cut -d ' ' -f 2 | cut -d '_' -f 2 | sort > $TMPDIR/$$.tmp && uniq $TMPDIR/$$.tmp && rm $TMPDIR/$$.tmp`,
+        "resultType" : "list"
+      });
+    })
+    .then((commandResult) => {
+      //console.log(commandResult);
+      ssh.dispose();
+      assert.equal(commandResult.code,0);
+      assert.isArray(commandResult.value);
+      assert.isNotEmpty(commandResult.stdout);
+      assert.isEmpty(commandResult.stderr);
+      assert.isUndefined(commandResult.signal);
+      done();
+    })
+    .catch(err => {
+      console.log(err);
+      ssh.dispose();
+      done(err);
+    });
+  });
 
 });
