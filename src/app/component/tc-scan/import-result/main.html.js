4e9afcaafeb23bc2c9d8ac5eebf4fa63ee36d887
raoul2000 <manu34@free.fr>
2018-02-01 15:57:28 +0100

    import-result ui

--- src/app/component/tc-scan/import-result/main.html.js
+++ src/app/component/tc-scan/import-result/main.html.js
@@ -1,39 +1,40 @@
 module.exports = `
 <div>
   <h3>Import Result</h3>
-  <table
-    v-for="tomcat in task.result.tomcats"
-    :key="tomcat._id"
-    :id="tomcat._id"
-    class="table">
+  <table  class="table">
     <tbody>
-      <tr>
-        <td>{{tomcat.id}}</td>
-        <td>
-          port : {{tomcat.port}}<br/>
-          Install Path : {{tomcat.installFolderPath}}<br/>
-          Webapp count : {{tomcat.webapps.length}}<br/>
-          <table>
-            <tr
-              v-for="webapp in tomcat.webapps"
-              :key="tomcat._id"
-              :id="tomcat._id"
-              >
-                <td>
-                  {{webapp.name}}
-                </td>
-                <td>
-                  <ul>
-                    <li>Context Path : {{webapp.contextPath}}</li>
-                    <li>Descriptor Path : {{webapp.descriptorFilePath}}</li>
-                    <li>Reference : {{webapp.refid}}</li>
-                  </ul>
-                </td>
-            </tr>
-          </table>
-
-        </td>
-      </tr>
+      <template
+        v-for="tomcat in task.result.tomcats"
+        :id="tomcat._id">
+        <tr style="background-color: #faf9ff;    border-top: 8px #f3f3f3 solid;">
+          <td><h4>{{tomcat.id}}</h4></td>
+          <td colspan="2">
+            <ul>
+              <li>port : {{tomcat.port}}</li>
+              <li>Install Path : <b>{{tomcat.installFolderPath}}</b></li>
+              <li>Webapp count : {{tomcat.webapps.length}}</li>
+            </ul>
+          </td>
+        </tr>
+        <template
+          v-for="webapp in tomcat.webapps"
+          :id="tomcat._id"
+        >
+          <tr>
+            <td></td>
+            <td>
+              <h5>{{webapp.name}}</h5>
+            </td>
+            <td style="font-size: 0.85em;">
+              <ul>
+                <li>Context Path : {{webapp.contextPath}}</li>
+                <li>Descriptor Path : {{webapp.descriptorFilePath}}</li>
+                <li>Reference : {{webapp.refId}}</li>
+              </ul>
+            </td>
+          </tr>
+        </template>
+      </template>
     </tbody>
   </table>
 
