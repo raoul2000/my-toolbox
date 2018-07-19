ce48ebfa60d99689cfecbb5f246d81e05888e58b
roaul2000 <manu34@free.fr>
2018-07-19 13:07:43 +0200

    change style on commands

--- src/app/component/desktop/item-view/commands/command-row/main.html.js
+++ src/app/component/desktop/item-view/commands/command-row/main.html.js
@@ -1,6 +1,6 @@
 module.exports = `
 <tr>
-  <td  style="white-space:nowrap;padding-right:2em;">
+  <td  width="15%" style="white-space:nowrap;padding-right:2em;">
     <inlineInput
       :initialValue="command.name"
       :valid="validation.name"
@@ -9,7 +9,7 @@ module.exports = `
       emptyValue="<em class='text-muted'>Enter command name ...</em>"
       v-on:changeValue="changeValue"/>
   </td>
-  <td width="100%">
+  <td>
     <inlineInput
       :initialValue="command.source"
       :valid="validation.source"
@@ -17,8 +17,6 @@ module.exports = `
       valueName="source"
       emptyValue="<em class='text-muted'>Enter your command ...</em>"
       v-on:changeValue="changeValue"/>
-      <br/>
-      result
       <div v-if="cmdResult && cmdResult.code === 0">
         <pre>{{cmdResult.stdout}}</pre>
       </div>
@@ -40,7 +38,6 @@ module.exports = `
       v-else-if="cmdResult && cmdResult.code !== 0"
       :title="cmdResult.stderr"
       class="glyphicon glyphicon-remove text-error" aria-hidden="true"></span>
-
     <span
       v-if="! runCmdTask || runCmdTask.status != 'BUSY'"
       v-on:click="runCommand"
@@ -51,14 +48,6 @@ module.exports = `
       title="command in progress ..."
       class="glyphicon glyphicon-refresh glyphicon-refresh-animate"
       aria-hidden="true" />
-
-
-
-    <span
-      v-on:click="editCommand()"
-      title="delete"
-      class="glyphicon glyphicon-pencil" aria-hidden="true"/>
-
     <span
       v-on:click="deleteCommand()"
       title="delete"
