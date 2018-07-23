1c07f04532b05f8dc406d9f692fb97e2466ca783
roaul2000 <manu34@free.fr>
2018-07-23 11:21:20 +0200

    add run-external

---
+++ src/app/lib/run-external.js
@@ -0,0 +1,11 @@
+const { spawn } = require('child_process');
+
+/**
+ * Run an external command
+ * 
+ * @param {string} cmdTemplate the command template string to execute
+ * @param {hash} values object values
+ */
+exports.run = function(cmdTemplate, values) {
+
+}
